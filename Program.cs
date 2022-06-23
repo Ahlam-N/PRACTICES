96b0c554d27c3df1435160c6674e420ec318f09b
Ahlam-N <107494447+Ahlam-N@users.noreply.github.com>
2022-06-23 18:21:57 +0300

    Practice part1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace variables
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+            /*  {
+                  byte number;
+                  number = 255;
+                  Console.WriteLine(number);
+              }
+            */
+            //Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            string number = "1234";
+            int num=Convert.ToInt32(number);
+            Console.WriteLine(number);
+        }
+    }
+}
